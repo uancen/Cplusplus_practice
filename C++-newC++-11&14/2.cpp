2ad85a1541b291c71c90f09910a45c0e6eeae7c4
yaopen <2194543625yao@gmail.com>
2022-04-26 09:28:25 +0800

    0426.2

--- C++-newC++-11&14/2.cpp
+++ C++-newC++-11&14/2.cpp
@@ -6,8 +6,16 @@ using namespace std;
 
 int main()
 {
-    int v1[] {1,2,3};
-    vector<int> v2 {1,2,3,4};
-    vector<string> s1 {"a","b","c","d"};
-    for(int a:v1) cout << a << endl;
+    int v1[]{1, 2, 3};
+    // vector(initializer_list<value_type> __l,
+    //        const allocator_type &__a = allocator_type())
+    //     : _Base(__a)
+    // {
+    //     _M_range_initialize(__l.begin(), __l.end(),
+    //                         random_access_iterator_tag());
+    // }
+    vector<int> v2{1, 2, 3, 4};
+    vector<string> s1{"a", "b", "c", "d"};
+    for (int a : v1)
+        cout << a << endl;
 }
